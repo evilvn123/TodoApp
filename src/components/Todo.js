07bd2f4205f8649d4d059b35e52aa0e7fffa6461
Duc Minh <taducminh2004@gmail.com>
2022-04-05 21:24:31 +0700

    init

---
+++ src/components/Todo.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const ToDo = ({}) => {
+  const [list, setList] = useState([
+    {
+      id: 1,
+      text: "Learn Javascript",
+      completed: false,
+    },
+    {
+      id: 2,
+      text: "Learn React",
+      completed: false,
+    },
+    {
+      id: 3,
+      text: "Build a React App",
+      completed: false,
+    },
+  ]);
+  return<div>
+    <input />
+    //<ToDoList list={list} />
+  </div>;
+};
+export default ToDo;
