cbcc95f2bec9e4906bce86969e3df7c3ab0ac2d3
Duc Minh <taducminh2004@gmail.com>
2022-06-03 19:51:17 +0700

    css signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,7 +1,31 @@
 import React from "react";
+import "../styles/Signin.css";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="signin">
+      <div className="formContainer">
+        <h2 className="title">Login to page</h2>
+        <form className="form">
+          <input
+            name="username"
+            // value={username}
+            type={"text"}
+            placeholder="Email or Username"
+            // onChange={onChangeUserName}
+          />
+          <input
+            name="password"
+            // value={password}
+            type={"password"}
+            placeholder="Password"
+            // onChange={onChangePassword}
+          />
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
