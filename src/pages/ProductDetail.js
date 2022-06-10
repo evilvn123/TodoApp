da37254a97159c93fd237093a70076141ffd47c8
Duc Minh <taducminh2004@gmail.com>
2022-06-10 20:42:01 +0700

    ui

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import products from "../__mockup/product";
-// import "../styles/ProductDetail.css";
+import "../styles/ProductDetail.css";
 
 const ProductDetail = () => {
   const { productId } = useParams();
@@ -10,7 +10,62 @@ const ProductDetail = () => {
     (item) => item.id === parseInt(productId)
   );
 
-  return <div>{productId}</div>;
+  return (
+    <div className="productDetail">
+      <div className="container">
+        <div className="images">
+          <img
+            className="main-image"
+            src={currentProduct.images[0]}
+            alt={currentProduct.name}
+          />
+          <div className="list-images">
+            {currentProduct.images.map((item, index) => (
+              <img key={index} src={item} alt={item} />
+            ))}
+          </div>
+        </div>
+        <div className="information">
+          <h4>{currentProduct.name}</h4>
+          <p>{currentProduct.description}</p>
+          <p>{currentProduct.price}</p>
+          <div className="size">
+            {currentProduct.sizes.map((item) => (
+              <span key={item}>
+                <input
+                  id={item}
+                  name="size"
+                  type={"radio"}
+                  value={item}
+                  // checked={item === size}
+                />
+                <label htmlFor={item}>{item}</label>
+              </span>
+            ))}
+          </div>
+          <div className="color">
+            {currentProduct.colors.map((item) => (
+              <span key={item}>
+                <input id={item} name="color" type={"radio"} value={item} />
+                <label htmlFor={item}>
+                  <div
+                    style={{
+                      backgroundColor: item,
+                      width: "15px",
+                      height: "15px",
+                      display: "inline-block",
+                      border: "solid 1px black",
+                    }}
+                  ></div>
+                </label>
+              </span>
+            ))}
+          </div>
+          <button>Thêm vào giỏ</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
