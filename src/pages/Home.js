28f8b775d18112a5b36c16214979ae624ff7aaa8
Duc Minh <taducminh2004@gmail.com>
2022-06-10 19:18:41 +0700

    prd card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import products from "../__mockup/product";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <img /> 
+    {/* banner */}
+    <div>
+      List product
+      {products.map((item) => (
+          <ProductCard key={item.id} product={item} />
+        ))}
+    </div>
+  </div>;
 };
 
 export default Home;
