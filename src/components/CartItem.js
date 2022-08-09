04200548ec39d0442bd04cef19785835e2e45f18
evilvn123 <taducminh2004@gmail.com>
2022-08-09 21:21:15 +0700

    change quantity

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/CartItem.css";
 
 const CartItem = ({ item }) => {
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  const onChangeInput = (value) => {
+    if (value < 0) {
+      setQuantity(0);
+    } else {
+      setQuantity(value);
+    }
+  };
+
+  useEffect(() => {
+    //Cập nhật lại local storage với giá trị quantity mới
+    const carts = JSON.parse(localStorage.getItem("cart")) || [];
+    const product = carts.find(
+      (element) =>
+        element.id === item.id &&
+        element.color === item.color &&
+        element.size === item.size
+    );
+    product.quantity = quantity;
+
+    localStorage.setItem("cart", JSON.stringify(carts));
+  }, [quantity]);
+
   return (
     <div className="item">
       <img className="image" src={item.image} alt={item.name} />
@@ -9,9 +33,13 @@ const CartItem = ({ item }) => {
       <span className="price">{+item.price} VND</span>
 
       <div className="quantity">
-        <button>-</button>
-        <input type="text" value={item.quantity} />
-        <button>+</button>
+        <button onClick={() => onChangeInput(quantity - 1)}>-</button>
+        <input
+          type="text"
+          value={quantity}
+          onChange={(event) => onChangeInput(event.target.value)}
+        />
+        <button onClick={() => onChangeInput(quantity + 1)}>+</button>
       </div>
 
       <span className="total">{+item.price * item.quantity} VND</span>
