3e15c7fb6fe676bd0f513f3d93ed2b9647cf52bd
evilvn123 <taducminh2004@gmail.com>
2022-08-09 20:50:01 +0700

    cartitem html, css

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,8 +3,18 @@ import "../styles/CartItem.css";
 
 const CartItem = ({ item }) => {
   return (
-    <div className="CartItem">
+    <div className="item">
+      <img className="image" src={item.image} alt={item.name} />
+      <span className="name">{item.name}</span>
+      <span className="price">{+item.price} VND</span>
 
+      <div className="quantity">
+        <button>-</button>
+        <input type="text" value={item.quantity} />
+        <button>+</button>
+      </div>
+
+      <span className="total">{+item.price * item.quantity} VND</span>
     </div>
   );
 };
